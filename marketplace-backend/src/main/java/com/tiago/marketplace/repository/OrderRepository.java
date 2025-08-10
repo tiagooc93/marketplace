993b154fe4df3dde79a82b367b07411ac627e5b7
Tiago Oliveira <toliveiracastro93@gmail.com>
2025-08-10 14:36:02 -0300

    refactor(backend): remove files from services payment and order from backend

--- marketplace-backend/src/main/java/com/tiago/marketplace/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tiago.marketplace.repository;
-
-import com.tiago.marketplace.model.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Orders, Long> {
-}
